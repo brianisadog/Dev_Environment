8cea374e306d3b3822d3c8c27fa35a97ef324078
brianisadog <s254901282002@gmail.com>
2018-07-16 17:13:31 -0700

    Typo fixed.

--- default.sh
+++ default.sh
@@ -10,10 +10,10 @@ apt-get update && apt-get upgrade -y \
     && apt-get install -y emacs curl openssh-server
 
 echo "Setting emacs' environment, using 4 spaces..."
-echo"(setq-default indent-tabs-mode nil)" >> ~/.emacs
-echo"(setq tab-width 4)" >> ~/.emacs
-echo"(setq-default c-basic-offset 4)" >> ~/.emacs
-echo"(setq make-backup-files nil)" >> ~/.emacs
+echo "(setq-default indent-tabs-mode nil)" >> ~/.emacs
+echo "(setq tab-width 4)" >> ~/.emacs
+echo "(setq-default c-basic-offset 4)" >> ~/.emacs
+echo "(setq make-backup-files nil)" >> ~/.emacs
 
 echo "Cleaning packages..."
 apt-get autoremove -y && apt-get autoclean -y
