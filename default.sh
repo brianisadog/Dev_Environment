19b96a78f87d1a050242efff7e0a92f85eb2e787
brianisadog <s254901282002@gmail.com>
2018-07-20 10:39:09 -0700

    Installation of Powerline-shell

--- default.sh
+++ default.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
-    echo "Please use sudo to run the script."
-    exit 1
-fi
+echo ">>> Proceeding update, upgrade, and installing default packages..."
+sudo apt-get update && sudo apt-get upgrade -y \
+    && sudo apt-get install -y emacs curl openssh-server
 
-echo "Proceeding update, upgrade, and installation..."
-apt-get update && apt-get upgrade -y \
-    && apt-get install -y emacs curl openssh-server
+echo ">>> Installing Python2.7 for Installing Powerline-shell..."
+sudo apt-get install -y python2.7 python-pip
 
-echo "Setting emacs' environment, using 4 spaces..."
-echo "(setq-default indent-tabs-mode nil)" >> ~/.emacs
+echo ">>> Setting emacs' environment, using 4 spaces..."
+echo "(setq-default indent-tabs-mode nil)" > ~/.emacs
 echo "(setq tab-width 4)" >> ~/.emacs
 echo "(setq-default c-basic-offset 4)" >> ~/.emacs
 echo "(setq make-backup-files nil)" >> ~/.emacs
 
-echo "Cleaning packages..."
-apt-get autoremove -y && apt-get autoclean -y
+echo ">>> Installing Fonts for Powerline..."
+git clone https://github.com/powerline/fonts.git --depth=1
+cd fonts/
+./install.sh
+cd ..
+rm -rf fonts/
+
+echo ">>> Installing Powerline-shell..."
+git clone https://github.com/b-ryan/powerline-shell
+cd powerline-shell/
+sudo python2.7 setup.py install
+
+if [ -e ~/.bashrc ]; then
+    echo ">>> Installing for Linux..."
+    export BASH_CONFIG="$HOME/.bashrc"
+else
+    echo ">>> Installing for OSX..."
+    export BASH_CONFIG="$HOME/.bash_profile"
+fi
+
+if ! grep -q "powerline-shell" $BASH_CONFIG; then
+    echo "function _update_ps1() {" >> $BASH_CONFIG
+    echo "    PS1=\$(powerline-shell \$?)" >> $BASH_CONFIG
+    echo "}" >> $BASH_CONFIG
+    echo "if [[ \$TERM != linux && ! \$PROMPT_COMMAND =~ _update_ps1 ]]; then" >> $BASH_CONFIG
+    echo "PROMPT_COMMAND=\"_update_ps1; \$PROMPT_COMMAND\"" >> $BASH_CONFIG
+    echo "fi" >> $BASH_CONFIG
+fi
+
+cd ..
+rm -rf powerline-shell/
+echo ">>> Powerline-shell has been installed, please restart the terminal."
+
+echo ">>> Cleaning packages..."
+sudo apt-get autoremove -y && sudo apt-get autoclean -y
