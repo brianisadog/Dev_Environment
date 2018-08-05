7530b00c08c3b2fc14f4d0635ccd0ffc7503c250
brianisadog <s254901282002@gmail.com>
2018-08-04 21:01:11 -0700

    Added megadecryptor into macos

--- macos.sh
+++ macos.sh
@@ -9,7 +9,7 @@ brew install python@2
 sudo easy_install pip
 
 echo ">>> Installing commonly-used packages..."
-brew install python3 emacs gcc openssh curl wget zsh
+brew install python3 emacs gcc openssh curl wget zsh nmap
 curl -O https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 rm -f get-pip.py
@@ -24,3 +24,13 @@ docker-machine create default --driver xhyve --xhyve-experimental-nfs-share
 eval $(docker-machine env default)
 docker-machine stop
 echo ">>> Docker installed successfully, to enable docker \"$ docker-machine start\""
+
+echo ">>> Installing megadecryptor..."
+git clone https://github.com/denysvitali/megadecrypter
+cd megadecrypter/
+brew install crystal
+crystal build --release src/megadecrypter.cr
+sudo mv ./megadecrypter /usr/local/bin/
+cd ..
+rm -rf megadecrypter
+brew uninstall libevent bdw-gc llvm@5 pcre crystal
